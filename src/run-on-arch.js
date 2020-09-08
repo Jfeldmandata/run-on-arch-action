446c24fe2cc31d8cd1dff28d82ae80bad8d2411c
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-07 22:47:53 -0500

    Hmm

--- src/run-on-arch.js
+++ src/run-on-arch.js
@@ -42,19 +42,19 @@ async function main() {
   }
 
   // Write install commands to a script file for running in the Dockerfile
-  const install = ```#!${shell}
-set -eu;
-export DEBIAN_FRONTEND=noninteractive;
-${core.getInput('install')}```;
+  const install = [
+    `#!${shell}`, 'set -eu;', 'export DEBIAN_FRONTEND=noninteractive;',
+    core.getInput('install'),
+  ].join('\n');
   fs.writeFileSync(
     path.join(__dirname, 'run-on-arch-install.sh'),
     install,
   );
 
   // Write container commands to a script file for running
-  const commands = ```#!${shell}
-set -eu;
-${core.getInput('run', { required: true })}```;
+  const commands = [
+    `#!${shell}`, 'set -eu;', core.getInput('run', { required: true }),
+  ].join('\n');
   fs.writeFileSync(
     path.join(__dirname, 'run-on-arch-commands.sh'),
     commands,
